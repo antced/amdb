c092bdff19b0cc750048c58491bcd76ac7d620ae
Anthony Cedrone <antcedrone95@gmail.com>
2022-11-30 20:01:54 -0500

    added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var moviePoster = document.getElementsByClassName("poster");
-var moviePosters = document.getElementById("moviePosters")
+var moviePosters = document.getElementById("moviePosters");
 
     moviePosters.addEventListener("dblclick", openImage);
 
@@ -9,3 +9,39 @@ var moviePosters = document.getElementById("moviePosters")
         var url=largeImage.getAttribute('src');
         window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     console.log("~I'M NOT DEAD YET~");
