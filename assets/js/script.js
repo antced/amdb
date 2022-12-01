480802f66d4ad5ca001a57c2798a1fd866e3ace9
Anthony Cedrone <antcedrone95@gmail.com>
2022-11-30 19:52:33 -0500

    open image in new tab on click

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var moviePoster = document.getElementsByClassName("poster");
+var moviePosters = document.getElementById("moviePosters")
+
+    moviePosters.addEventListener("dblclick", openImage);
+
+
+    function openImage(event) {
+        var largeImage = event.target;
+        var url=largeImage.getAttribute('src');
+        window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
+     }
