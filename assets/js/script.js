7728e61f6a2c7c957d3243b6bfd7ffbf1fb91395
Anthony Cedrone <antcedrone95@gmail.com>
2022-12-06 09:27:27 -0500

    added final image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var moviePoster = document.getElementsByClassName("poster");
 var moviePosters = document.getElementById("moviePosters");
+const searchBtn = document.getElementById("search");
 
     moviePosters.addEventListener("dblclick", openImage);
+    searchBtn.addEventListener("submit", searchGo);
 
 
     function openImage(event) {
@@ -9,6 +11,17 @@ var moviePosters = document.getElementById("moviePosters");
         var url=largeImage.getAttribute('src');
         window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
      }
+
+    function searchGo(event) {
+        event.preventDefault();
+        const input= event.target[0].value;
+        if (input.toLowerCase() === "morbius") {
+            console.log("you win!");
+            window.open("./assets/images/morbiusalex.jpg", "Image", "width=largeImage.stylewidth,height=largeImage.style.height,resizable=1");
+        } else {
+            console.log("try again, bitch");
+        }
+    }
 
 
 
